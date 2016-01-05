0f04cd0bb0ace3cb391e64c231433d7b8dfc9034
Cam Crews <i@camcre.ws>
2016-01-05 15:53:08 -0500

    Captcha implementation
    captcha.rb - generate_exclusion_array, generate_word_array, number_of_words_to_exclude
    spec/captcha_spec.rb - verifies captcha.rb functionality

---
+++ captcha.rb
@@ -0,0 +1,44 @@
+# captcha.rb 
+# implements functional requirements for implementing captcha tests on text data
+#
+# 
+
+# Generate Exclusion Array
+# Returns an exclusion list from a body of text
+#
+# @param text the body of text to analyze
+# @param n desired size of exclusion list to return (optional)
+# @returns an array exclusion list
+def generate_exclusion_array( text, n = nil ) 
+  a_w = generate_word_array(text)
+  a_w.sample(number_of_words_to_exclude(a_w.length,n)) 
+end
+
+
+# Generate Word Array
+# Converts string into an array of unique downcase words with punctuation removed
+#
+# @param text the body of text to analyze
+# @returns an array of words
+def generate_word_array( text )
+  return [] unless text 
+  text.downcase.split(" ").map{|w| w.tr('.?!-;:"()[]{}/','')}.uniq  # load array of unique words w/ downcase conversion
+  # 1) convert to downcase, 2) split into array, 3) remove punctuation, 4) remove duplicates
+end
+
+# Number Of Words To Exclude
+# Determines how many words to exclude based upon the size of the unique word array
+# This number will be a random number between 1 and half the number of total elements
+# unless there are one or less total words, in whicih case it will be 0
+#
+# @param size of the word array
+# @param desired size of the exclusion array (optional)
+# @returns the desired size of the exclusion array
+def number_of_words_to_exclude( size, desired = nil )
+  return 0 if size <= 1 || size.class != Fixnum # if we don't have a number or enough elements, no exclusion array
+  if( desired.class == Fixnum && size > desired ) # only allow desired size if provided AND it's smaller than total word array size
+    desired
+  else
+    (1..(size/2)).to_a.sample  if size > 1 # else, let's specify an exclusion array size of at least 1 up to half the total number of words
+  end
+end
---
+++ spec/captcha_spec.rb
@@ -0,0 +1,90 @@
+require "./spec/spec_helper"
+
+describe 'Within the Captcha system,' do
+
+  let(:text){ "Here is some text for you to look at.  It is text that I wrote myself, right?" }
+  let(:text_len){ 15 }
+  let(:a_text){ %w(here is some text for you to look at it that i wrote myself right) }
+
+  context "generate_word_array" do
+    it "returns an array upon receipt of text" do
+      expect(generate_word_array(text).class).to eq(Array)
+    end
+
+    it "returns the correct number of unique words" do 
+      expect(generate_word_array(text).length).to eq(text_len)
+    end
+
+    it "returns the correct array" do
+      expect(generate_word_array(text)).to eq(a_text)
+    end
+  end
+
+  context "number_of_words_to_exclude" do
+    let(:a_len){ [15, 1, 0, 2, 6] }
+
+    it "should return a number between 1 and 7 given a size of 15" do
+      expect(number_of_words_to_exclude(a_len[0])).to satisfy {|v| v >= 1 && v <= 7 }
+    end
+
+    it "should return 0 given a size of 1" do
+      expect(number_of_words_to_exclude(a_len[1])).to eq(0) 
+    end
+
+    it "should return 0 given a size of 0" do
+      expect(number_of_words_to_exclude(a_len[2])).to eq(0) 
+    end
+
+    it "should return 1 given a size of 2" do
+      expect(number_of_words_to_exclude(a_len[3])).to eq(1) 
+    end
+
+    it "should return a number between 1 and 3 given a size of 6" do
+      expect(number_of_words_to_exclude(a_len[4])).to satisfy {|v| v >= 1 && v <= 3 }
+    end
+  end
+
+  context "generate_exclusion_array" do
+    let(:s_one){ "lonely" }
+    let(:s_two){ "best friends" }
+    let(:s_empty){ "" }
+    let(:s_spaces){ "        " }
+    let(:s_repeated){ "pretty please please please Please Please please please" }
+    let(:s_ex1){ "It was the best of times, it was the worst of times.  Okay, that seems like a contradiction." }
+    let(:s_ex2){ "The holidays wound up absorbing almost all available time, funny how that works, right?"}
+
+    it "should return an empty exclusion array from a one-word paragraph" do
+      expect(generate_exclusion_array(s_one)).to eq([])
+    end
+
+    it "should return a single element array from a two-word paragraph" do
+      expect(generate_exclusion_array(s_two)).to satisfy{|a| a == ["best"] || a == ["friends"] }
+    end
+
+    # we should consider this an error case and handle it 
+    it "should return an empty exclusion array from an empty paragraph" do
+      expect(generate_exclusion_array(s_empty)).to eq([])
+    end
+
+    it "should return an empty exclusion array if given only spaces" do
+      expect(generate_exclusion_array(s_empty)).to eq([])
+    end
+
+    it "should return a single element array from a paragraph with two unique words" do
+      expect(generate_exclusion_array(s_repeated)).to satisfy{|a| a == ["pretty"] || a == ["please"] }
+    end
+
+    it "should return an exclusion array from between 1-6 elements for a paragraph with 13 unique words" do
+      expect(generate_exclusion_array(s_ex1)).to satisfy{|a| a.length >= 1 && a.length <= 6 }
+    end
+
+    it "should return an exclusion array from between 1-7 elements for a paragraph with 14 unique words" do
+      expect(generate_exclusion_array(s_ex2)).to satisfy{|a| a.length >= 1 && a.length <= 7 }
+    end
+
+    it "should return an exclusion array of 4 elements for a paragraph with 14 unique words when we specify 4 elements" do
+      expect(generate_exclusion_array(s_ex2,4)).to satisfy{|a| a.length == 4 }
+    end
+  end
+
+end 
