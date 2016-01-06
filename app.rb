15cb7ffd6618b74e0231f79d591467c92a42452d
Cam Crews <i@camcre.ws>
2016-01-06 13:19:38 -0500

    Captcha POST data handling
    Template for response: post.json.erb
    app.rb uses verify_word_count

--- app.rb
+++ app.rb
@@ -19,7 +19,16 @@ end
 
 # process a user's response
 post '/captcha' do
-
+  # need to receive 1) the source_text, 2) exclusion list, 3) user guess, 4) authenticity hash
+  result = verify_word_count params[:source_text], params[:exclude], params[:guess]
+  if result
+    status 200
+    msg = ["Great job, you got it right!","I always knew you were human, sorry for putting you through that.","Looks good to me.","Thanks!  You got it!", "Great Scot, you did it!","Great Ceasers ghost, you really are human!","Your counting skills are impressive."]
+  else # guess found to not match, error case
+    status 400
+    msg = ["You done goofed!","Nope, that ain't right!","Want to check again?","Can you even count?","That was pretty off.","Epic fail! I knew you were a robot!"]
+  end
+  erb :"post.json", locals: { source_text: params[:source_text], exclude: params[:exclude], guess: params[:guess], result: result, msg: msg.sample }
 end
 
 def load_source_text()
